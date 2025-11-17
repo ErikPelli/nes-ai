4ff5bdc622c00186e35d0e6f362ba65df1deb149
Erik Pellizzon <erikpellizzon1@gmail.com>
2025-11-17 22:30:27 +0100

    Add initial MLP weights definition

---
+++ mlp/weights.c
@@ -0,0 +1,10 @@
+#include <fix16.h>
+
+#include "weights.h"
+
+const fix16_t WEIGHTS[] = {
+    F16(1),
+    F16(2),
+    F16(3),
+    F16(4)
+};
---
+++ mlp/weights.h
@@ -0,0 +1,8 @@
+#ifndef NES_AI_WEIGHTS_H
+#define NES_AI_WEIGHTS_H
+
+#include <fix16.h>
+
+extern const fix16_t WEIGHTS[];
+
+#endif // NES_AI_WEIGHTS_H
